820b40e1a6fed5f5e82e21465a7d21bc914bb708
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2023-11-17 00:08:38 +1100

    Add install-linux-apps.sh

---
+++ .scripts/setup/linux/install-linux-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+add-apt-repository ppa:neovim-ppa/unstable
+
+apt-get install neovim
+apt-get install ripgrep
+apt-get install build-essential
+apt-get install build-essential --fix-missing
+apt-get install unzip
+apt-get install tmux
+apt-get install golang-go
+
