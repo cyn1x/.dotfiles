261c983d2ce1cd7223e798711a8fe1fa3b8e0c17
Zac Vukovic <zac.vukovic@tafensw.edu.au>
2023-11-17 10:50:26 +1100

    Change to absolute path for mklink cmd

--- .scripts/setup.bat
+++ .scripts/setup.bat
@@ -1,5 +1,5 @@
 @echo off
 
-mklink /h .gitconfig %USERPROFILE%\.dotfiles\.gitconfig
+mklink /h %USERPROFILE%\.gitconfig %USERPROFILE%\.dotfiles\.gitconfig
 mklink /d %USERPROFILE%\.nvim\plugins\outhouse.nvim D:\Dev\GitHub\outhouse.nvim
 mklink /d %LOCALAPPDATA%\nvim %USERPROFILE%\.dotfiles\.config\nvim
