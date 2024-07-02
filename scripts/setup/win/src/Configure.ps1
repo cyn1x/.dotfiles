5f456bd6947588d53c830bb40de326d4660f7064
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2024-07-02 15:17:04 +1000

    Add Win configure script

---
+++ scripts/setup/win/src/Configure.ps1
@@ -0,0 +1,28 @@
+# Remove searchbox from taskbar
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force
+
+# Uninstall default programs
+Start-Process powershell.exe 'Get-AppxPackage *Microsoft.People* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+Start-Process powershell.exe 'Get-AppxPackage *WebExperience* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+Start-Process powershell.exe 'Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+Start-Process powershell.exe 'Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+Start-Process powershell.exe 'Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+Start-Process powershell.exe 'Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+Start-Process powershell.exe 'Get-AppxPackage *Disney* | Remove-AppxPackage' -NoNewWindow -PassThru -wait
+
+# Remove Startup Items
+Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "Steam" -ErrorAction SilentlyContinue
+
+# Remove Desktop Shortcut Icons
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+
+Get-ChildItem $DesktopPath -Filter *.lnk | Foreach-Object {
+    $file = $_
+    $path = Join-Path -Path $DesktopPath -ChildPath $file
+    
+    Remove-Item $path
+}
+
+# Disable Cortana & Bing Search
+Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /V "CortanaEnabled" /T REG_DWORD /D 0 /F
+Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /V "BingSearchEnabled" /T REG_DWORD /D 0 /F
