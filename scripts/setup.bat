24ce3c22879b6322ae0b9578ba63a81c0810563a
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2025-06-30 10:11:46 +1000

    Updated setup.bat paths to reflect new drive structure

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -2,5 +2,6 @@
 
 mklink /h %USERPROFILE%\.gitconfig %USERPROFILE%\.dotfiles\.gitconfig
 mklink /d %USERPROFILE%\.dotfiles\scripts\setup\win\etc %USERPROFILE%\.config
-mklink /d %USERPROFILE%\.nvim\plugins\outhouse.nvim D:\Dev\GitHub\cyn1x\outhouse.nvim
+mklink /d %USERPROFILE%\.nvim\plugins\outhouse.nvim %USERPROFILE%\Dev\GitHub\cyn1x\outhouse.nvim
 mklink /d %LOCALAPPDATA%\nvim %USERPROFILE%\.dotfiles\.config\nvim
+mklink /d C:\Dev %USERPROFILE%\Dev
